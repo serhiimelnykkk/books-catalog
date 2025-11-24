f58777f07cd8bd4616838d86dcc94922aa8c63e9
smrlyman <smrlyman@gmail.com>
2025-11-24 22:40:37 +0200

    feat: add "showFavoritesOnly" variable to BookCatalog; replace temp storage to separate file

---
+++ src/storage/temp.ts
@@ -0,0 +1,39 @@
+const temporaryStorage = [
+  {
+    id: 1,
+    name: "1984",
+    coverName: "/images/1984.jpg",
+  },
+  {
+    id: 2,
+    name: "The Catcher In The Rye The Catcher In The Rye",
+    coverName: "/images/the_catcher_in_the_rye.jpg",
+  },
+  {
+    id: 3,
+    name: "The Great Gatsby",
+    coverName: "/images/the_great_gatsby.jpg",
+  },
+  {
+    id: 4,
+    name: "To Kill A Mockingbird",
+    coverName: "/images/to_kill_a_mockingbird.jpg",
+  },
+  {
+    id: 5,
+    name: "Animal Farm",
+    coverName: "/images/animal_farm.jpg",
+  },
+  {
+    id: 6,
+    name: "East of Eaden",
+    coverName: "/images/east_of_eaden.jpg",
+  },
+  {
+    id: 7,
+    name: "Fahrenheit 451",
+    coverName: "/images/fahrenheit_451.jpg",
+  },
+];
+
+export default temporaryStorage;
