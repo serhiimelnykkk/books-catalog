f06d93fdd54a65e55bc20959e98581d27f3664ce
smrlyman <smrlyman@gmail.com>
2025-11-24 21:35:27 +0200

    feat: Add "Add to Favorite button"

--- src/components/common/BookCard/BookCard.tsx
+++ src/components/common/BookCard/BookCard.tsx
@@ -1,16 +1,47 @@
+import { useEffect, useState } from "react";
 import styles from "./BookCard.module.css";
 
 interface BookCardProps {
+  bookId: number;
   bookCoverSrc: string;
   bookCoverAlt: string;
   bookName: string;
+  bookIds: string[];
+  onBookIdsChange: React.Dispatch<React.SetStateAction<string[]>>;
 }
 
-const BookCard = ({ bookCoverSrc, bookCoverAlt, bookName }: BookCardProps) => {
+const BookCard = ({
+  bookCoverSrc,
+  bookCoverAlt,
+  bookName,
+  bookId,
+  onBookIdsChange,
+  bookIds,
+}: BookCardProps) => {
+  const [favorite, setFavorite] = useState(
+    bookIds.some((value) => value == bookId)
+  );
+
+  const handleBookAdd = () => {
+    onBookIdsChange((prev) => [...prev, bookId.toString()]);
+  };
+
+  const handleBookRemove = () => {
+    onBookIdsChange((prev) => prev.filter((id) => id !== bookId.toString()));
+  };
+
+  const handleButtonClick = () => {
+    favorite ? handleBookRemove() : handleBookAdd();
+    setFavorite(!favorite);
+  };
+
   return (
     <div className={styles.div}>
       <img className={styles.img} src={bookCoverSrc} alt={bookCoverAlt} />
       <p className={styles.p}>{bookName}</p>
+      <button className={styles.btn} onClick={handleButtonClick}>
+        {favorite ? "Remove from Favorites" : "Add to Favorites"}
+      </button>
     </div>
   );
 };
--- src/components/pages/BookCatalog/BookCatalog.tsx
+++ src/components/pages/BookCatalog/BookCatalog.tsx
@@ -1,48 +1,68 @@
+import { useEffect, useState } from "react";
 import BookCard from "../../common/BookCard/BookCard";
 
 import styles from "./BookCatalog.module.css";
 
 const temporaryStorage = [
   {
+    id: 1,
     name: "1984",
     coverName: "/images/1984.jpg",
   },
   {
-    name: "The Catcher In The Rye",
+    id: 2,
+    name: "The Catcher In The Rye The Catcher In The Rye",
     coverName: "/images/the_catcher_in_the_rye.jpg",
   },
   {
+    id: 3,
     name: "The Great Gatsby",
     coverName: "/images/the_great_gatsby.jpg",
   },
   {
+    id: 4,
     name: "To Kill A Mockingbird",
     coverName: "/images/to_kill_a_mockingbird.jpg",
   },
   {
+    id: 5,
     name: "Animal Farm",
     coverName: "/images/animal_farm.jpg",
   },
   {
+    id: 6,
     name: "East of Eaden",
     coverName: "/images/east_of_eaden.jpg",
   },
   {
+    id: 7,
     name: "Fahrenheit 451",
     coverName: "/images/fahrenheit_451.jpg",
   },
 ];
 
 const BookCatalog = () => {
+  const [bookIds, setBookIds] = useState<string[]>(
+    JSON.parse(localStorage.getItem("bookIds") || "[]")
+  );
+
+  useEffect(() => {
+    const newBookIds = JSON.stringify(bookIds);
+    localStorage.setItem("bookIds", newBookIds);
+  }, [bookIds]);
+
   return (
     <div className={styles.wrapper}>
       <section className={styles.section}>
         {temporaryStorage.map((book) => (
           <BookCard
-            key={book.name}
+            key={book.id}
+            bookId={book.id}
             bookName={book.name}
             bookCoverSrc={book.coverName}
             bookCoverAlt={book.name}
+            bookIds={bookIds}
+            onBookIdsChange={setBookIds}
           />
         ))}
       </section>
