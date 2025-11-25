cefee162f7fa8382740ea2fff6a067b144b24930
smrlyman <smrlyman@gmail.com>
2025-11-25 14:30:26 +0200

    feat: Add message box showed when catalog is empty

--- src/components/pages/BookCatalog/BookCatalog.tsx
+++ src/components/pages/BookCatalog/BookCatalog.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import BookCard from "../../common/BookCard/BookCard";
 import temporaryStorage from "../../../storage/temp";
 import styles from "./BookCatalog.module.css";
+import { Link } from "react-router";
 
 interface BookCatalogProps {
   showFavoritesOnly?: boolean;
@@ -22,21 +23,39 @@ const BookCatalog = ({ showFavoritesOnly }: BookCatalogProps) => {
     : temporaryStorage;
 
   return (
-    <div className={styles.wrapper}>
-      <section className={styles.section}>
-        {storageToMap.map((book) => (
-          <BookCard
-            key={book.id}
-            bookId={book.id}
-            bookName={book.name}
-            bookCoverSrc={book.coverName}
-            bookCoverAlt={book.name}
-            bookIds={bookIds}
-            onBookIdsChange={setBookIds}
-          />
-        ))}
-      </section>
-    </div>
+    <>
+      {storageToMap.length > 0 ? (
+        <div className={styles.wrapper_main}>
+          <section className={styles.section}>
+            {storageToMap.map((book) => (
+              <BookCard
+                key={book.id}
+                bookId={book.id}
+                bookName={book.name}
+                bookCoverSrc={book.coverName}
+                bookCoverAlt={book.name}
+                bookIds={bookIds}
+                onBookIdsChange={setBookIds}
+              />
+            ))}
+          </section>
+        </div>
+      ) : (
+        <div className={styles.wrapper_empty}>
+          {showFavoritesOnly ? (
+            <p className={styles.message}>
+              No books here. Return to{" "}
+              <Link to="/" className={styles.return_to_catalog}>
+                catalog
+              </Link>{" "}
+              to add some
+            </p>
+          ) : (
+            <p className={styles.message}>No books available</p>
+          )}
+        </div>
+      )}
+    </>
   );
 };
 
