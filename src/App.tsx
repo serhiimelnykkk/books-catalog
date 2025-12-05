09a55018f89b8602a0d1f4eb8367e085b561d848
smrlyman <smrlyman@gmail.com>
2025-12-05 19:13:06 +0200

    feat: Add routing to book page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { Routes, Route } from "react-router";
 import Header from "./components/common/Header/Header";
 import NotFound from "./components/pages/NotFound/NotFound";
+import Book from "./components/pages/Book/Book";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           path="/favorites"
           element={<BookCatalog showFavoritesOnly={true} />}
         />
+        <Route path="/books/:id" element={<Book />} />
 
         <Route path="*" element={<NotFound />} />
       </Routes>
