65e3b1a44191b1dfe41bb4e66053647e5f8a3184
smrlyman <smrlyman@gmail.com>
2025-12-10 11:50:53 +0200

    feat: add book filtering

--- src/components/pages/BookCatalog/BookCatalog.tsx
+++ src/components/pages/BookCatalog/BookCatalog.tsx
@@ -1,7 +1,12 @@
-import BookCard from "../../common/BookCard/BookCard";
-import temporaryStorage from "../../../storage/temp";
-import styles from "./BookCatalog.module.css";
+import { useMemo } from "react";
+import { useForm } from "react-hook-form";
 import { Link } from "react-router";
+import useDebounce from "../../../hooks/useDebounce";
+import temporaryStorage from "../../../storage/temp";
+import { GENRES, type Book } from "../../../types";
+import BookCard from "../../common/BookCard/BookCard";
+import styles from "./BookCatalog.module.css";
+import filters from "./filters";
 import useFavorites from "./hooks/useFavorites";
 
 interface BookCatalogProps {
@@ -15,12 +20,50 @@ const BookCatalog = ({ showFavoritesOnly }: BookCatalogProps) => {
     ? temporaryStorage.filter((book) => favorites.includes(book.id))
     : temporaryStorage;
 
+  const { register, watch } = useForm<Book>();
+
+  const filterValues = watch();
+
+  const debouncedJson = useDebounce(JSON.stringify(filterValues), 500);
+
+  const filteredBooks = useMemo(() => {
+    const debouncedFilterValues = JSON.parse(debouncedJson);
+
+    return books.filter((book) =>
+      Object.keys(debouncedFilterValues).every((key) => {
+        const filterKey = key as keyof Book;
+
+        const filter = filters[filterKey];
+
+        if (!filter) {
+          return true;
+        }
+
+        return filter(book, debouncedFilterValues);
+      })
+    );
+  }, [debouncedJson, books]);
+
   return (
     <>
-      {books.length > 0 ? (
+      <form>
+        <input type="text" placeholder="book name" {...register("name")} />
+        <input type="text" placeholder="book author" {...register("author")} />
+        <input
+          type="text"
+          placeholder="book year published"
+          {...register("year_published", { valueAsNumber: true })}
+        />
+        <select multiple {...register("genres")}>
+          {GENRES.map((genre) => (
+            <option key={genre}>{genre}</option>
+          ))}
+        </select>
+      </form>
+      {filteredBooks.length > 0 ? (
         <div className={styles.wrapper_main}>
           <section className={styles.book_catalog}>
-            {books.map((book) => (
+            {filteredBooks.map((book) => (
               <BookCard
                 key={book.id}
                 book={book}
